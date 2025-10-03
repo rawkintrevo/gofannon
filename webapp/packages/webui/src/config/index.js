76c78e214bc0d47fb2c7d9b601ebba2984ae8dd2
Trevor Grant <trevor.d.grant@gmail.com>
2025-10-03 17:09:04 -0500

    Revert "339 - Create Agent Flow (#340)" (#341)
    This reverts commit ada6270da408f26e902349ad631e7e0022c458b3.

--- webapp/packages/webui/src/config/index.js
+++
@@ -1,3 +0,0 @@
-// This file makes the shared config available to the webui package
-import config from '@gofannon/config';
-export default config;
--- webapp/packages/webui/src/theme.js
+++
@@ -1,6 +0,0 @@
-import { createTheme } from '@mui/material/styles';
-import appConfig from './config';
-
-const theme = createTheme(appConfig.theme);
-
-export default theme;
